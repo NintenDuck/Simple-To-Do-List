5a226819a1f1540e381e627573364a6aa1d6b625
NintenDuck <andrecked507@gmail.com>
2022-07-20 15:22:22 -0500

    Add: button can change to add or delete

--- scripts/lowerButton.gd
+++ scripts/lowerButton.gd
@@ -1,3 +1,19 @@
 extends Button
 
+enum btnType {
+	ADD,
+	DELETE
+}
 
+onready var buttonType = btnType.ADD
+
+
+func _on_Button_pressed():
+	pass # Replace with function body.
+
+
+func change_button_type(typeOfButton:String = "+"):
+	if typeOfButton == '+':
+		text = '+'
+	else:
+		text = '-'
