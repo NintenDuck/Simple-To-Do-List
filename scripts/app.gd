ac1c707538475d192984442b8b7ed5a6564fe2a9
NintenDuck <andrecked507@gmail.com>
2022-07-20 17:47:50 -0500

    Fix: TaskContainer correctly scales & wraps text

--- scripts/app.gd
+++ scripts/app.gd
@@ -5,15 +5,15 @@ onready var lowButton = $ButtonMarginContainer/ButtonContainer/Button
 
 
 func _ready():
-    lowButton.connect("pressed", self, "buttonPressed")
-    taskContainer.connect("num_tasks_changed", self, "check_button_change")
+	lowButton.connect("pressed", self, "buttonPressed")
+	taskContainer.connect("num_tasks_changed", self, "check_button_change")
 
 func buttonPressed():
-    taskContainer.delete_checked_tasks()
+	taskContainer.delete_checked_tasks()
 
 
 func check_button_change(tasksSelected):
-    if tasksSelected >= 1:
-        print("Boton debe eliminar tasks")
-    else:
-        print("Boton debe añadir tasks")
+	if tasksSelected >= 1:
+		print("Boton debe eliminar tasks")
+	else:
+		print("Boton debe añadir tasks")
